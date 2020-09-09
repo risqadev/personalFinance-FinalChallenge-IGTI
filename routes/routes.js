32307787001faab23e9aa450d4280f94a9a1c7a7
Ricardo Camacho <camacho.rc@gmail.com>
2020-09-09 03:35:57 -0300

    getTransactions not completed

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const { getTransactions, errorHandler } = require('../services/transactionService');
 const transactionRouter = express.Router();
+
+transactionRouter.get('/', getTransactions);
+
+// tratamento de erros
+transactionRouter.use(errorHandler);
 
 module.exports = transactionRouter;
--- services/transactionService.js
+++ services/transactionService.js
@@ -6,3 +6,35 @@ const ObjectId = mongoose.Types.ObjectId;
 // o servidor é Linux, isso faz diferença. Gastei umas boas horas tentando
 // descobrir esse erro :-/
 const TransactionModel = require('../models/TransactionModel');
+
+async function getTransactions(request, response, next) {
+  const yearMonth = request.query.period;
+
+  try {
+    const transactions = await TransactionModel.find({ yearMonth });
+
+    // console.log(transactions);
+
+    response.send(transactions);
+
+  } catch (error) {
+    return next(error);
+  }
+}
+
+/* function getAllTransactions(request, response, next) {
+  try {
+    throw new Error('Teste');
+  } catch (error) {
+    return next(error);
+  }
+} */
+
+// tratamento de erros
+function errorHandler(error, request, response, _next) {
+  console.log(`Error: ${request.method} ${request.baseUrl}${request.url}: ${error.message}`);
+
+  return response.status(400).send({ error: error.message });
+}
+
+module.exports = { errorHandler, getTransactions };
